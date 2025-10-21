35160ea229c987f7dfb7bd01e9fb997c53865c0a
Valentina <svistunova.vv@yandex.com>
2025-10-21 10:51:15 +0300

    Configure warehouse

--- commerce/warehouse/src/main/java/ru/yandex/practicum/Main.java
+++ commerce/warehouse/src/main/java/ru/yandex/practicum/Main.java
@@ -2,10 +2,12 @@ package ru.yandex.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
-public class WarehouseApp {
+public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(WarehouseApp.class, args);
+        SpringApplication.run(Main.class, args);
     }
 }
